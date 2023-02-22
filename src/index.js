bda80c7936982a2725dfa2085f22c427f1fbb5b4
Ibragim-Saitov <122826573+Ibragim-Saitov@users.noreply.github.com>
2023-02-22 12:40:37 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,31 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import {List} from './components/List';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+function App() {
+   return(
+      <List title = 'INFO'/>
+   )
+};
 
-
-class MouseTracker extends React.Component {
-    constructor(props) {
-      super(props);
-      this.handleMouseMove = this.handleMouseMove.bind(this);
-      this.state = { x: 0, y: 0 };
-    }
-  
-    handleMouseMove(event) {
-      this.setState({
-        x: event.clientX,
-        y: event.clientY
-      });
-    }
-  
-    render() {
-      return (
-        <div style={{ height: '100vh' }} onMouseMove={this.handleMouseMove}>
-          <h1>Move the mouse around!</h1>
-          <p>The current mouse position is ({this.state.x}, {this.state.y})</p>
-        </div>
-      );
-    }
-  }
+export default App

de89d1166221332b1a1340dc2224376b2476b37b
Ibragim-Saitov <122826573+Ibragim-Saitov@users.noreply.github.com>
2023-02-22 12:43:12 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,34 +1,9 @@
-import React from "react";
-import logo from './logo.svg';
-import './App.css';
-import Fcomponent from './Fcomponent';
-import Ccomponent from './Ccomponent';
-import Menu from "./Menu";
-
+import {List} from './components/List';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <Fcomponent name="Ibrahim"/>
-        <Ccomponent name="Ali"/>
-
-        <img src= {logo} className="App-logo" alt="logo"/>
-        <p>
-          Web Developer Blog
-        </p>
-        <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-        >
-        Web Developer Blog
-        </a>
-      </header>
-    </div>
-  );
-}
-
+   return(
+      <List title = 'INFO'/>
+   )
+};
 
 export default App;
